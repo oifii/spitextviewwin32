eb82b9a714a8b12d50c1f17ba24a7ef4a59dd02b
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 15:08:25 -0500

    first commit

---
+++ Release/spitextviewwin32.bat
@@ -0,0 +1,11 @@
+rem start spitextviewwin32.exe "testcode.cpp" 180.0 0.25 600 300 700 500 220 1 0 0 12
+rem start spitextviewwin32.exe "testcode.cpp" 180.0 0.050 600 300 700 500 220 1 0 0 12
+rem start spitextviewwin32.exe "testcode.cpp" 30.0 0.000 600 300 700 500 220 1 0 0 12
+rem start spitextviewwin32.exe "testcode.cpp" 30.0 0.000 1 1 300 1000 220 1 0 0 12
+rem start spitextviewwin32.exe "testcode.cpp" 30.0 0.000 1 1 300 1000 220 1 0 0 48
+rem start spitextviewwin32.exe "testcode.cpp" 30.0 0.000 1 1 1900 1000 220 1 0 0 48
+rem start spitextviewwin32.exe "testcode.cpp" -1 0.000 1 1 1900 1000 220 1 0 0 24
+rem start spitextviewwin32.exe "testcode.cpp" -1 0.025 1 1 1900 1000 220 1 0 0 24
+rem start spitextviewwin32.exe "testcode.cpp" -1 0.050 1 1 900 1000 220 1 0 0 24
+start spitextviewwin32.exe "testcode.cpp" -1 0.0250 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "begin - Copy.ahk" "end - Copy.ahk"
+rem start spitextviewwin32.exe "testcode.cpp" -1 0.0 1 1 900 1000 220 1 0 0 24
